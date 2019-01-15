a485723a3980759a66b66fcb971fd9308eec58cc
Volizik <kizilov.yura@gmail.com>
2019-01-16 01:02:12 +0300

    desktop header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import {IconSpriteModule} from 'ng-svg-icon-sprite';
+
+import {AppComponent} from './app.component';
+import {ButtonComponent} from './components/button/button.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {TopbarComponent} from './components/topbar/topbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ButtonComponent,
+        MenuComponent,
+        TopbarComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        IconSpriteModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
