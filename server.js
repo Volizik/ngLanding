0177fabd6c28d9d0cac510b41b13b09a4c8936d1
Volizik <kizilov.yura@gmail.com>
2019-02-19 23:02:02 +0300

    express gzip

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const app = express();
 
-app.use(express.static('./dist/abz'));
+app.use(compression());
+app.use('/', express.static('./dist/abz'));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname, '/dist/abz/index.html'));
