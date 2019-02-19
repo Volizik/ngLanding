fbe80871c6a596d22ece57a2128e2e19f0393059
Volizik <kizilov.yura@gmail.com>
2019-02-20 00:02:35 +0300

    fix node error

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const compression = require('compression');
 const app = express();
 
 app.use(compression());
-app.use('/', express.static('./dist/abz'), { maxAge: '30 days' });
+app.use('/', express.static('./dist/abz', { maxAge: '30 days' }));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname, '/dist/abz/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
