61386904d808f4a98e950dbd7499297641c543ad
Volizik <kizilov.yura@gmail.com>
2019-02-19 23:51:15 +0300

    add caching

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require('compression');
 const app = express();
 
 app.use(compression());
-app.use('/', express.static('./dist/abz'));
+app.use('/', express.static('./dist/abz'), { maxAge: '30 days' });
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname, '/dist/abz/index.html'));
